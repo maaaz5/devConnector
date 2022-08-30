407534e46150132ab7e752ba408d44c721cc8adf
Mohamed Amine Azirgui <69221403+maaaz5@users.noreply.github.com>
2022-08-30 16:30:44 +0100

    auth

---
+++ client/src/App.js
@@ -0,0 +1,35 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./components/layout/Navbar";
+import Landing from "./components/layout/Landing";
+import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
+import Alert from "./components/layout/Alert";
+import "./App.css";
+import { Fragment } from "react";
+//redux
+import { Provider } from "react-redux";
+import store from "./store";
+
+function App() {
+  return (
+    <Provider store={store}>
+      <Router>
+        <Fragment>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Landing />} />
+          </Routes>
+          <section className="container">
+            <Alert />
+            <Routes>
+              <Route path="/register" element={<Register />} />
+              <Route path="/login" element={<Login />} />
+            </Routes>
+          </section>
+        </Fragment>
+      </Router>
+    </Provider>
+  );
+}
+
+export default App;
